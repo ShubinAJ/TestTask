ee20b96ffdefe702863d495f1e1f92e13285f4aa
Александр <a_shubin94@mail.ru>
2024-04-06 21:45:53 +0300

    Добавьте файлы проекта.

---
+++ FileManager.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Globalization;
+
+namespace TestTask
+{
+    internal class FileManager
+    {
+        public string logPath;
+        public string outputPath;
+        //IPAddress startAdress;
+        //IPAddress endAdress;
+        public int startAdress;
+        public int endAdress;
+        public DateOnly timeStart;
+        public DateOnly timeEnd;
+        public string[] parameters;
+
+        string[] sourceFileArr;
+        public int stage = 0;
+
+        //string[] source;
+        //StringBuilder source = new StringBuilder();
+        List<string> cmdLineArgs = new List<string>();
+        //public FileManager(string str)
+        public FileManager(string[] str)
+
+        {
+            ////source = str;
+            ////parameters = source.Split("--");
+            //parameters = str.Split("--").ToArray();
+            parameters = str;
+
+
+            for (int i = 0; i < parameters.Length; i++)
+            {
+                parameters[i] = parameters[i].Trim();
+
+                if (parameters[i].StartsWith(','))
+                {
+                    parameters[i] = parameters[i].Substring(1);
+                }
+                if (parameters[i].EndsWith(','))
+                {
+                    parameters[i] = parameters[i].Substring(0, parameters[i].Length - 1);
+                }
+
+            }
+
+
+            if (parameters.Length == 5)
+            {
+                logPath = parameters[1];
+                outputPath = parameters[2];
+                timeStart = DateOnly.ParseExact(parameters[3], "dd.MM.yyyy", CultureInfo.InvariantCulture);
+                timeEnd = DateOnly.ParseExact(parameters[4], "dd.MM.yyyy", CultureInfo.InvariantCulture);
+            }
+            if (parameters.Length == 6)
+            {
+                logPath = parameters[1];
+                outputPath = parameters[2];
+                startAdress = Int32.Parse(parameters[3]);
+                timeStart = DateOnly.ParseExact(parameters[4], "dd.MM.yyyy", CultureInfo.InvariantCulture);
+                timeEnd = DateOnly.ParseExact(parameters[5], "dd.MM.yyyy", CultureInfo.InvariantCulture);
+            }
+            if (parameters.Length == 7)
+            {
+                logPath = parameters[1];
+                outputPath = parameters[2];
+                startAdress = Int32.Parse(parameters[3]);
+                endAdress = Int32.Parse(parameters[4]);
+                timeStart = DateOnly.ParseExact(parameters[5], "dd.MM.yyyy", CultureInfo.InvariantCulture);
+                timeEnd = DateOnly.ParseExact(parameters[6], "dd.MM.yyyy", CultureInfo.InvariantCulture);
+            }
+
+        }
+
+
+        //public FileManager(StringBuilder sb)
+        //{
+        //    source = sb;
+        //    parameters = source.ToString().Split("--");
+        //    if (parameters.Length == 4)
+        //    {
+        //        logPath = parameters[0];
+        //        outputPath = parameters[1];
+        //        timeStart = DateOnly.ParseExact(parameters[2], "dd.MM.yyyy", CultureInfo.InvariantCulture); 
+        //        timeEnd = DateOnly.ParseExact(parameters[3], "dd.MM.yyyy", CultureInfo.InvariantCulture); 
+        //    }
+        //    if (parameters.Length == 5)
+        //    {
+        //        logPath = parameters[0];
+        //        outputPath = parameters[1];
+        //        //startAdress = parameters[2];
+        //        startAdress = Int32.Parse(parameters[2]);
+        //        timeStart = DateOnly.ParseExact(parameters[3], "dd.MM.yyyy", CultureInfo.InvariantCulture);
+        //        timeEnd = DateOnly.ParseExact(parameters[4], "dd.MM.yyyy", CultureInfo.InvariantCulture);
+        //    }
+        //    if (parameters.Length == 6)
+        //    {
+        //        logPath = parameters[0];
+        //        outputPath = parameters[1];
+        //        startAdress = Int32.Parse(parameters[2]);
+        //        endAdress = Int32.Parse(parameters[3]);
+        //        timeStart = DateOnly.ParseExact(parameters[4], "dd.MM.yyyy", CultureInfo.InvariantCulture);
+        //        timeEnd = DateOnly.ParseExact(parameters[5], "dd.MM.yyyy", CultureInfo.InvariantCulture);
+        //    }
+
+        //}
+
+
+
+        
+
+
+        public bool Check (string str)
+        {
+            string source = str;
+            if (source != null) 
+            {
+                logPath = source;
+                return true;
+
+            }
+            else return false;
+        }
+
+        public string[] GetFile(string src)
+        {
+            logPath = src;
+            try
+            {
+                if (File.Exists(logPath))
+                {
+                    sourceFileArr = File.ReadAllLines(logPath);
+                    Console.WriteLine("Файл успешно считан");
+                    stage++;
+                    
+                }
+                else
+                {
+                    Console.WriteLine("Исходный файл не найден");
+                    
+                }
+
+            }
+            catch (Exception e)
+            {
+
+                //Console.WriteLine("Исходный файл не найден");
+            }
+            return sourceFileArr;
+        }
+        public string[] Sort(string[] src)
+        {
+            //this.startAdress = 
+            string[] files = new string[1];
+            return files;
+        }
+
+        //public void GetFile (string src)
+        //{
+        //    logPath = src;
+        //    try
+        //    {
+        //        if (File.Exists(logPath))
+        //        {
+        //            sourceFileArr = File.ReadAllLines(logPath);
+        //            Console.WriteLine("Файл успешно считан");
+        //            stage++;
+        //        }
+        //        else
+        //        {
+        //            Console.WriteLine("Исходный файл не найден");
+        //        }
+
+        //    }
+        //    catch (Exception e)
+        //    {
+
+        //        //Console.WriteLine("Исходный файл не найден");
+        //    }
+
+        //}
+
+        public void CreateFile (string dst)
+        {
+            outputPath = dst;
+            try
+            {
+                //sourceFileArr = File.ReadAllLines(logPath);
+                File.AppendAllLines(outputPath, sourceFileArr);
+                Console.WriteLine("Файл успешно создан");
+                stage++;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Некорректный путь к файлу назначения");
+            }
+        }
+        public void CreateStartDate (string str)
+        {
+            try
+            {
+                timeStart = DateOnly.ParseExact(str, "dd.MM.yyyy", CultureInfo.InvariantCulture);
+                stage++;
+            }
+            catch (Exception e) 
+            {
+                Console.WriteLine("Дата введена некорректно");
+            }
+        }
+        public void CreateEndDate(string str)
+        {
+            try
+            {
+                timeEnd = DateOnly.ParseExact(str, "dd.MM.yyyy", CultureInfo.InvariantCulture);
+                stage++;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Дата введена некорректно");
+            }
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace TestTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //для отладки
+            Console.WriteLine("--C:\\Users\\Александр\\Desktop\\source.txt");
+            Console.WriteLine("--C:\\Users\\Александр\\Desktop\\output.txt");
+            Console.WriteLine("--23.02.2023");
+            Console.WriteLine("--12.05.2024");
+            Console.WriteLine("--C:\\Users\\Александр\\Desktop\\source.txt--C:\\Users\\Александр\\Desktop\\output.txt--23.02.2023--12.05.2024");
+            Console.WriteLine("--C:\\Users\\Александр\\Desktop\\source.txt_--C:\\Users\\Александр\\Desktop\\output.txt --23.02.2023 --12.05.2024");
+            Console.WriteLine("--C:\\Users\\Александр\\Desktop\\source.txt--C:\\Users\\Александр\\Desktop\\output.txt --7--23.02.2023 --12.05.2024");
+            Console.WriteLine("--C:\\Users\\Александр\\Desktop\\source.txt--C:\\Users\\Александр\\Desktop\\output.txt --7 --10 --23.02.2023 --12.05.2024");
+
+
+
+            string[] envArgsList = Environment.CommandLine.Split("--").ToArray();
+            string[] consoleArgsList = Console.ReadLine().Split("--").ToArray();
+
+            //FileManager fm = new FileManager(envArgsList);
+            //FileManager fm1 = new FileManager(consoleArgsList);
+
+            //string envArgsList = Environment.CommandLine;
+            //string consoleArgsList = Console.ReadLine();
+            if (envArgsList.Length > consoleArgsList.Length)
+            {
+                FileManager fm = new FileManager(envArgsList);
+                Show(fm);
+                fm.GetFile(fm.logPath);
+                fm.CreateFile(fm.outputPath);
+
+                //Console.WriteLine("envArgsList");
+                //for (int i = 0; i < fm.parameters.Length; i++)
+                //{
+                //    Console.WriteLine(fm.parameters[i]);
+                //}
+            }
+            if (envArgsList.Length < consoleArgsList.Length)
+            {
+                FileManager fm = new FileManager(consoleArgsList);
+                Show(fm);
+                fm.GetFile(fm.logPath);
+                fm.CreateFile(fm.outputPath);
+            }
+            if (envArgsList.Length == consoleArgsList.Length)
+            {
+                FileManager fm = new FileManager(consoleArgsList);
+                Show(fm);
+                fm.GetFile(fm.logPath);
+                fm.CreateFile(fm.outputPath);
+            }
+
+            void Show(FileManager f)
+            {
+                Console.WriteLine(f.logPath);
+                Console.WriteLine(f.outputPath);
+                Console.WriteLine(f.startAdress);
+                Console.WriteLine(f.endAdress);
+                Console.WriteLine(f.timeStart);
+                Console.WriteLine(f.timeEnd);
+            }
+
+
+
+
+            Console.WriteLine("Нажмите любую клавишу для выхода");
+            Console.ReadKey();
+        }
+    }
+}
